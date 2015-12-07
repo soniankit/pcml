28e08dbb0bd4c4f78fbe5a12985c2e7e2d093313
Ankit <asoni@umn.edu>
2015-12-07 16:07:26 -0600

    Adding session changes according to the new pip requirements

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ Authors and contributors: Eric Shook (eshook@kent.edu); Zhengliang Feng (odayfan
 """
 from setuptools import setup
 from pip.req import parse_requirements
-
+import pip
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements("./requirements.txt")
+install_reqs = parse_requirements("./requirements.txt", session=pip.download.PipSession())
 
 # reqs is a list of requirement
 reqs = [str(ir.req) for ir in install_reqs]
